83f330fd254fb0d87f76c5ce2287b948e814868c
Michał <enedil.isildur@gmail.com>
2020-06-04 16:13:21 +0200

    rm unused commented code

--- client/radio-client.cc
+++ client/radio-client.cc
@@ -4,18 +4,6 @@
 #include "args.h"
 #include "telnetserver.h"
 #include "message.h"
-/*
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    sockaddr_in addr;
-    addr.sin_addr.s_addr = htonl(0);
-    addr.sin_port = htons(5004);
-    KeepAlive kal(sock);
-    kal.sendPipeMessage(Command::NoAddress);
-    sleep(5);
-    kal.sendPipeMessage(addr);
-    sleep(5);
-    kal.sendPipeMessage(Command::Stop);
-*/
 
 int main(int argc, char* argv[]) {
     Args args(argc, argv);
